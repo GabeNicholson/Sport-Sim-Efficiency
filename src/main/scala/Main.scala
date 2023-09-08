6dd9eb0d098ec3b7b0224fde31079a4c1a60ce31
gabenicholson <gabenicholson@uchicago.edu>
2023-09-08 11:18:53 -0400

    feat: Add Main file

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,19 @@
+object Main extends App {
+  /**
+   * Creates teams and plays them against each other.
+   */
+  val Gabe = new Team("Gabe", 85)
+  val Shana = new Team("Shana", 50)
+  val Andrew = new Team("Andrew", 50)
+  val Dim = new Team("Dim", 75)
+  val Dim2 = new Team("Dim2", 60)
+  val Dim3 = new Team("Dim3", 55)
+  val allTeams = List(Gabe, Shana, Andrew, Dim, Dim2, Dim3)
+
+  val tourney = new Tournament(allTeams)
+  //    seasonScores is a List containing the Team instances who won.
+  val seasonScores = tourney.simulateRegularSeason()
+  val playoffTeams = tourney.getTopNTeams(seasonScores, 2)
+  val winner = tourney.simulatePlayoffMatch(playoffTeams).name
+  println(s"$winner won the tournament!")
+}
